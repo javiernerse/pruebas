d230e51969b31374d1bc8608eb75cbc721214fe3
javiernerse <javners@hotmail.com>
2026-07-01 23:52:30 -0300

    borre invenrario

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import funciones
-import time
-##from colorama import Fore,init
-
-##init()
-
-tecla= ""
-while(tecla=="") :
- funciones.linea_verde()
- time.sleep(5)
- funciones.linea_negra()
-
- tecla=input()
+vacio
